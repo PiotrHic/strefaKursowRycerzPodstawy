e9029d9cc397f3a1b566ee7958be1d80f2100b73
piotrhic <piotrhic@gmail.com>
2023-02-28 20:11:43 +0100

    Initial project - Closes #1.

---
+++ src/main/java/com/example/strefakursowrycerzpodstawy/StrefaKursowRycerzPodstawyApplication.java
@@ -0,0 +1,14 @@
+package com.example.strefakursowrycerzpodstawy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StrefaKursowRycerzPodstawyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StrefaKursowRycerzPodstawyApplication.class, args);
+        System.out.println("Dziala!");
+    }
+
+}
---
+++ src/test/java/com/example/strefakursowrycerzpodstawy/StrefaKursowRycerzPodstawyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.strefakursowrycerzpodstawy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StrefaKursowRycerzPodstawyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
